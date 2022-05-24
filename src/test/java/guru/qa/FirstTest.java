2aea01461fc41ce9a61db7646a035c3828a3b295
Daniil Glushenkov <glushenkov.daniel@yandex.ru>
2022-05-25 00:20:13 +0300

    change operator to >=

--- src/test/java/guru/qa/FirstTest.java
+++ src/test/java/guru/qa/FirstTest.java
@@ -6,9 +6,9 @@ import org.junit.jupiter.api.Test;
 public class FirstTest {
 @Test
     void firstTest() {
-    Assertions.assertTrue(3>2);
+    Assertions.assertTrue(3>=2);
 }
     void secondTest() {
-        Assertions.assertTrue(3>1);
+        Assertions.assertTrue(3>=1);
     }
 }
